bad20ace709439617c56f73893bdfb87f5113c28
subaru744 <32183595+subaru744@users.noreply.github.com>
2017-10-18 11:53:11 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 'use strict';
-/**
-* 与えられた自然数の階乗を返す
-* 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
-* @param {Number} n
-* @returns {Number}
-*/
-function factorial(n) {
-    let result = 1;
-    // TODO このコメントを消して正しく実装してください。
-    return result;
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+memo.set(2, 1);
+function trib(n) {
+    if (memo.has(n)) {
+        return memo.get(n);
+    }
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
+    memo.set(n, value);
+    return value;
 }
-const assert = require('assert');
-assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
-assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
-assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
-assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
-console.log('すべてのテストを通過しました');
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(trib(i));
+}
